96461fa270b3b1fee431946e994913d376adb687
Thomas Riga <thomasriga@gmail.com>
2012-09-10 08:58:49 +0200

    first release

--- arduino/smallbot/smallbot.ino
+++ arduino/smallbot/smallbot.ino
@@ -5,10 +5,11 @@ Servo right_servo;
 int green_led_pin = 13;
 int yellow_led_pin = 12;
 int red_led_pin = 11;
-int left_servo_pin = A0;
-int right_servo_pin = A1;
+int right_servo_pin = A0;
+int left_servo_pin = A1;
 int trigger_pin = A2;
 int echo_pin = A3;
+int dir_flag = 0;
 
 void setup() { 
   left_servo.attach(left_servo_pin); 
@@ -18,44 +19,83 @@ void setup() {
   pinMode(red_led_pin, OUTPUT); 
   pinMode(trigger_pin, OUTPUT);
   pinMode(echo_pin, INPUT);  
+  red_blink(300); 
+  yellow_blink(300);
+  green_blink(300);
 } 
 
-void loop() { 
+void loop() {
+  long cm = 0, min_dist = 20;
+  cm = microsec_to_cm(microsec_srf04());
+  while(cm < min_dist) {
+    if(dir_flag) {
+      rotate_left_msec(50);
+    }
+    else {
+      rotate_right_msec(50);
+    }
+    cm = microsec_to_cm(microsec_srf04());
+  }
+  if(dir_flag) {
+    dir_flag = 0;
+  }
+  else {
+    dir_flag = 1;
+  }
+  forward_msec(200);
+}
+
+void simple_3_way_max_dist() {
+  long msec1, msec2, msec3;
+  all_blink(500);
+  rotate_left_msec(500);
+  msec1 = microsec_srf04();
+  rotate_right_msec(500);
+  msec2 = microsec_srf04();
+  rotate_right_msec(500);
+  msec3 = microsec_srf04();
+  if(msec1 > msec2) {
+    if(msec1 > msec3) {
+      // left wins
+      rotate_left_msec(1000);
+      green_blink(500);
+    }
+  }
+  if(msec2 > msec1) {
+    if(msec2 > msec3) {
+      // center wins
+      rotate_left_msec(500);
+      yellow_blink(500);
+    }
+  }
+  if(msec3 > msec1) {
+    if(msec3 > msec1) {
+      // right wins, do nothing
+      red_blink(500);
+    }
+  }
+  forward_msec(1000);
+  delay(20);
+}
+
+void test() { 
   long msec, cm, wait;
-  digitalWrite(green_led_pin, HIGH);   
-  delay(300);               
-  digitalWrite(green_led_pin, LOW);    
-  digitalWrite(yellow_led_pin, HIGH);   
-  delay(300);               
-  digitalWrite(yellow_led_pin, LOW);    
-  digitalWrite(red_led_pin, HIGH);   
-  delay(300);               
-  digitalWrite(red_led_pin, LOW);                  
   msec = microsec_srf04();
   cm = microsec_to_cm(msec);
-  wait = cm * 100;
-  digitalWrite(green_led_pin, HIGH);   
-  digitalWrite(red_led_pin, HIGH);   
-  digitalWrite(yellow_led_pin, HIGH);   
-  delay(wait);               
-  digitalWrite(green_led_pin, LOW);    
-  digitalWrite(red_led_pin, LOW);   
-  digitalWrite(yellow_led_pin, LOW);   
-  delay(1000); 
- 
-  left_servo.writeMicroseconds(1700);  
-  delay(2000);                      
-  left_servo.writeMicroseconds(1300);  
-  delay(2000);
-  left_servo.writeMicroseconds(1500);  
-  delay(2000); 
-  right_servo.writeMicroseconds(1700);  
-  delay(2000);                      
-  right_servo.writeMicroseconds(1300);  
-  delay(2000);
-  right_servo.writeMicroseconds(1500);  
-  delay(2000); 
-  
+  wait = cm * 10;
+  all_blink(wait); 
+  green_blink(500);
+  left_forward_msec(2000);
+  green_blink(500);
+  left_backward_msec(2000);
+  yellow_blink(500);
+  right_forward_msec(2000);
+  yellow_blink(500);
+  right_backward_msec(2000);
+  red_blink(500);
+  rotate_left_msec(2000);
+  red_blink(500);
+  rotate_right_msec(2000);
 }
 
 long microsec_srf04() {
@@ -72,4 +112,129 @@ long microsec_srf04() {
 long microsec_to_cm(long microsec)
 {
   return microsec / 29l / 2l;
+}
+
+void forward() {
+  left_forward();
+  right_forward();
+}
+
+void backward() {
+  left_backward();
+  right_backward();
+}
+
+void forward_msec(int msec) {
+  forward();
+  delay(msec);
+  stop();
+}
+
+void backward_msec(int msec) {
+  backward();
+  delay(msec);
+  stop();
+}
+
+void left_forward() {
+  left_servo.writeMicroseconds(1700);  
+}
+
+void left_backward() {
+  left_servo.writeMicroseconds(1300);  
+}
+
+void left_stop() {
+  left_servo.writeMicroseconds(1500);  
+}
+
+void right_forward() {
+  right_servo.writeMicroseconds(1300);  
+}
+
+void right_backward() {
+  right_servo.writeMicroseconds(1700);  
+}
+
+void right_stop() {
+  right_servo.writeMicroseconds(1500);  
+}
+
+void left_forward_msec(int msec) {
+  left_forward();
+  delay(msec);
+  left_stop();  
+}
+
+void left_backward_msec(int msec) {
+  left_backward();
+  delay(msec);
+  left_stop();  
+}
+
+void right_forward_msec(int msec) {
+  right_forward();
+  delay(msec);
+  right_stop();  
+}
+
+void right_backward_msec(int msec) {
+  right_backward();
+  delay(msec);
+  right_stop();  
+}
+
+void stop() {
+  left_stop();
+  right_stop();
+}
+
+void rotate_left() {
+  left_backward();
+  right_forward();
+}
+
+void rotate_right() {
+  right_backward();
+  left_forward();
+}
+
+void rotate_left_msec(int msec) {
+  rotate_left();
+  delay(msec);
+  stop();
+}
+
+void rotate_right_msec(int msec) {
+  rotate_right();
+  delay(msec);
+  stop();
+}
+
+void green_blink(int msec) {
+  digitalWrite(green_led_pin, HIGH);   
+  delay(msec);               
+  digitalWrite(green_led_pin, LOW);    
+}
+
+void yellow_blink(int msec) {
+  digitalWrite(yellow_led_pin, HIGH);   
+  delay(msec);               
+  digitalWrite(yellow_led_pin, LOW);    
+}
+
+void red_blink(int msec) {
+  digitalWrite(red_led_pin, HIGH);   
+  delay(msec);               
+  digitalWrite(red_led_pin, LOW);    
+}
+
+void all_blink(int msec) {
+  digitalWrite(green_led_pin, HIGH);   
+  digitalWrite(yellow_led_pin, HIGH);   
+  digitalWrite(red_led_pin, HIGH);   
+  delay(msec);               
+  digitalWrite(green_led_pin, LOW);    
+  digitalWrite(yellow_led_pin, LOW);    
+  digitalWrite(red_led_pin, LOW);    
 }
